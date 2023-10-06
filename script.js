3580d1fb5b1bc4532caaeb3b9cc857eb09266791
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-06 21:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function palindrome(str){
 
 	let str = str.replace(/[^a-zA-Z0-9]/g,"").tolowerCase();
 
-	return s === s.split("").reverse().join("");
+	return str === str.split("").reverse().join("");
 	
 }
 module.exports = palindrome
